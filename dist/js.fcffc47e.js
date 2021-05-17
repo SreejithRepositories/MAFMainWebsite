2dabdfcf7897534a25e34d84904eb8539996dacb
Sreejith <jithoo123@gmail.com>
2021-05-17 11:46:02 +0530

    contact us responsive images

--- dist/js.fcffc47e.js
+++ dist/js.fcffc47e.js
@@ -43126,7 +43126,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57802" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61341" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

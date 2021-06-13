2adf3a335ed8ba363444af00707e81c6bcedd507
Sreejith <jithoo123@gmail.com>
2021-06-13 09:33:59 +0530

    deleted index.html and added index2.html

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,26 @@
-var m2j = require("markdown-to-json");
+// var convertToHtml = require("markdown-to-html-converter");
+
 var fs = require("fs");
 var path = require("path");
-
-fs.readdir(path.join(__dirname, "../posts/blog"));
+var md2json = require("md-2-json");
+var showdown = require("showdown");
+const dirname = path.join(__dirname, "../posts/blog");
+const dirname1 = path.join(__dirname, "../posts/blog/");
+var filenames = [];
+converter = new showdown.Converter();
+fs.readdir(dirname, (err, files) => {
+  if (err) console.log(err);
+  else {
+    console.log(files);
+    files.forEach((file) => {
+      fs.readFile(dirname1 + file, "utf8", (err, data) => {
+        if (err) {
+          return console.log(err);
+        }
+        // console.log(data);
+        // console.log(md2json.parse(data));
+        console.log(converter.makeHtml(data));
+      });
+    });
+  }
+});
